4d43144892d3238703815f9f0ce964308619a9b7
Meghana <meghanamanchal@gmail.com>
2024-12-09 18:17:02 +0530

    build memory game

--- script.js
+++ script.js
@@ -52,15 +52,59 @@ const cardArrray = [
 cardArrray.sort(()=>0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard(){
     for(let i=0;i<cardArrray.length;i++){
         const card = document.createElement('img')
         card.setAttribute('src','images/blank.png')
         card.setAttribute('data-id',i)
+        card.addEventListener('click',flipCard)
         gridDisplay.appendChild(card)
+
 
     }
 }
 createBoard()
+
+function flipCard(){
+   let cardId =  this.getAttribute('data-id')
+   cardsChosen.push(cardArrray[cardId].name)
+   cardsChosenIds.push(cardId)
+   this.setAttribute('src',cardArrray[cardId].img)
+   if(cardsChosen.length == 2){
+    setTimeout(checkMatch,500)
+   }
+}
+
+function checkMatch(){
+    const cards = document.querySelectorAll('img')
+    const optionOneID = cardsChosenIds[0]
+    const optionTwoID = cardsChosenIds[1]
+    if(optionOneID == optionTwoID){
+        alert('You have clicked the same image!')
+    }
+
+    if(cardsChosen[0] == cardsChosen[1]){
+        alert('you found the match')
+        cards[optionOneID].setAttribute('src','images/blank.png')
+        cards[optionTwoID].setAttribute('src','images/blank.png')
+        cards[optionOneID].removeEventListener('click',flipCard)
+        cards[optionTwoID].removeEventListener('click',flipCard)
+        cardsWon.push(cardsChosen)
+    }else{
+        cards[optionOneID].setAttribute('src','images/blank.png')
+        cards[optionTwoID].setAttribute('src','images/blank.png')
+        alert('Sorry try again')
+    }
+    resultDisplay.textContent = cardsWon.length;
+    cardsChosen = []
+    cardsChosenIds = []
+    if(cardsWon.length == (cardArrray.length/2)){
+        resultDisplay.innerHTML = 'Congratulations you found all!'
+    }
+}
 
