126e819fe2aced5235bdefc872e85e3c5f128758
Meghana <meghanamanchal@gmail.com>
2024-12-09 16:56:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const cardArrray = [
+    {
+        name:'fries',
+        img:'images/fries.png'
+    },
+    {
+        name:'cheeseburger',
+        img:'images/cheeseburger.png'
+    },
+    {
+        name:'hotdog',
+        img:'images/hotdog.png'
+    },
+    {
+        name:'ice-cream',
+        img:'images/ice-cream.png'
+    },
+    {
+        name:'milkshake',
+        img:'images/milkshake.png'
+    },
+    {
+        name:'pizza',
+        img:'images/pizza.png'
+    },
+    {
+        name:'fries',
+        img:'images/fries.png'
+    },
+    {
+        name:'cheeseburger',
+        img:'images/cheeseburger.png'
+    },
+    {
+        name:'hotdog',
+        img:'images/hotdog.png'
+    },
+    {
+        name:'ice-cream',
+        img:'images/ice-cream.png'
+    },
+    {
+        name:'milkshake',
+        img:'images/milkshake.png'
+    },
+    {
+        name:'pizza',
+        img:'images/pizza.png'
+    }
+]
+
+cardArrray.sort(()=>0.5 - Math.random());
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard(){
+    for(let i=0;i<cardArrray.length;i++){
+        const card = document.createElement('img')
+        card.setAttribute('src','images/blank.png')
+        card.setAttribute('data-id',i)
+        gridDisplay.appendChild(card)
+
+    }
+}
+createBoard()
+
